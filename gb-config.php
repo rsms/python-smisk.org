0950a57cfcc95edb7fd9a04fa77e84aeaa0d3be5
Rasmus Andersson <rasmus@notion.se>
2009-08-28 17:36:10 +0200

    corrected path to secret

--- gb-config.php
+++ gb-config.php
@@ -3,7 +3,7 @@
 gb::$site_title = 'smisk';
 
 # Private secret
-gb::$secret = trim(file_get_contents('secret'));
+gb::$secret = trim(file_get_contents(dirname(__FILE__).'/secret'));
 
 # Index prefix -- rewrite rules only engaged live
 if ($_SERVER['SERVER_NAME'] === 'python-smisk.org')
