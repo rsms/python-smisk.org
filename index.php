a8c05963a84d602d10ddc29df0774a56fbb54f70
Rasmus Andersson <rasmus@notion.se>
2009-08-28 18:05:46 +0200

    SP -> TAB

--- index.php
+++ index.php
@@ -28,39 +28,39 @@ function selected($startswith) {
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=2.0; minimum-scale=1.0; user-scalable=1;" />
-    <link rel="stylesheet" media="screen" href="<?= gb::$site_url ?>res/screen.css" type="text/css" />
-    <link rel="stylesheet" media="only screen and (max-device-width: 480px)" href="<?= gb::$site_url ?>res/iphone.css" type="text/css" />
-    <link rel="icon" href="<?= gb::$site_url ?>res/favicon.png" type="image/png"/>
-    <link rel="home" href="<?= gb::$site_url ?>" />
-    <link rel="alternate" title="Changes (commits)" type="application/atom+xml" href="http://github.com/feeds/rsms/commits/smisk/" />
-    <link rel="alternate" title="Announcements" type="application/rss+xml" href="http://groups.google.com/group/smisk-announce/feed/rss_v2_0_msgs.xml" />
-    <link rel="alternate" title="Discussions" type="application/rss+xml" href="http://groups.google.com/group/smisk-discuss/feed/rss_v2_0_msgs.xml" />
-    <title><?= gb_title() ?></title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=2.0; minimum-scale=1.0; user-scalable=1;" />
+		<link rel="stylesheet" media="screen" href="<?= gb::$site_url ?>res/screen.css" type="text/css" />
+		<link rel="stylesheet" media="only screen and (max-device-width: 480px)" href="<?= gb::$site_url ?>res/iphone.css" type="text/css" />
+		<link rel="icon" href="<?= gb::$site_url ?>res/favicon.png" type="image/png"/>
+		<link rel="home" href="<?= gb::$site_url ?>" />
+		<link rel="alternate" title="Changes (commits)" type="application/atom+xml" href="http://github.com/feeds/rsms/commits/smisk/" />
+		<link rel="alternate" title="Announcements" type="application/rss+xml" href="http://groups.google.com/group/smisk-announce/feed/rss_v2_0_msgs.xml" />
+		<link rel="alternate" title="Discussions" type="application/rss+xml" href="http://groups.google.com/group/smisk-discuss/feed/rss_v2_0_msgs.xml" />
+		<title><?= gb_title() ?></title>
 	<? gb_head() ?>
-  </head>
-  <body>
-    <div id="head">
-      <h1><?= gb_title() ?></h1>
-    </div>
-    <div id="menu">
-      <ul>
-        <li><a href="<?= gb::$site_url ?>" <?= selected('about') ?>
-          title="An overview of Smisk, list of key features and mailing lists">about</a></li>
-        <li><a href="<?= gb::url_to('/examples') ?>" <?= selected('examples') ?>
-          title="Introduction to Smisk by example">examples</a></li>
-        <li><a href="<?= gb::url_to('/docs') ?>" <?= selected('docs') ?>
-          title="Documentation of the Python API, the C API as well as general usage and guidelines">docs</a></li>
-        <li><a href="<?= gb::url_to('/downloads') ?>" <?= selected('downloads') ?>
-          title="How and where to download Smisk">downloads</a></li>
-        <li><a href="http://github.com/rsms/smisk/issues"
-          title="List of all the horrible bugs... or wait a minute... no bugs?!">issues</a></li>
-        <li><a href="http://github.com/rsms/smisk/tree/master"
-          title="The master repository at GitHub, including all source code and information about the current development process">code</a></li>
-      </ul>
-    </div>
+	</head>
+	<body>
+		<div id="head">
+			<h1><?= gb_title() ?></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li><a href="<?= gb::$site_url ?>" <?= selected('about') ?>
+					title="An overview of Smisk, list of key features and mailing lists">about</a></li>
+				<li><a href="<?= gb::url_to('/examples') ?>" <?= selected('examples') ?>
+					title="Introduction to Smisk by example">examples</a></li>
+				<li><a href="<?= gb::url_to('/docs') ?>" <?= selected('docs') ?>
+					title="Documentation of the Python API, the C API as well as general usage and guidelines">docs</a></li>
+				<li><a href="<?= gb::url_to('/downloads') ?>" <?= selected('downloads') ?>
+					title="How and where to download Smisk">downloads</a></li>
+				<li><a href="http://github.com/rsms/smisk/issues"
+					title="List of all the horrible bugs... or wait a minute... no bugs?!">issues</a></li>
+				<li><a href="http://github.com/rsms/smisk/tree/master"
+					title="The master repository at GitHub, including all source code and information about the current development process">code</a></li>
+			</ul>
+		</div>
 		<?
 		
 		if (gb::$is_page && !gb::$is_404) {
@@ -79,5 +79,5 @@ function selected($startswith) {
 		?>
 
 	<? gb_footer() ?>
-  </body>
+	</body>
 </html>
