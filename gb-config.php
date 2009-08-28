17e4cf882ba8fbec23fbae7b4e06b79f0db82de4
Rasmus Andersson <rasmus@notion.se>
2009-08-28 17:29:54 +0200

    moved secret and ignored it as well as some other cleanup

--- gb-config.php
+++ gb-config.php
@@ -1,17 +1,11 @@
 <?
 # Site title and description
 gb::$site_title = 'smisk';
-#gb::$site_description = 'Ramblings by My Name';
 
-# Shared secret used for hook triggering, etc.
-# Must contain only shell-safe characters (a-zA-Z0-9_-. and so on) and be at
-# least 62 bytes long.
-gb::$secret = '2ft2x8pmlxkaqpmrvb8uckwxzbnk4bmio5427lbvlifik6jokgvkk260yknabtrzff';
+# Private secret
+gb::$secret = trim(file_get_contents('secret'));
 
-# If you have applied server rewrite rules, routing requests to index.php, you
-# probably want to set index_url to "" (the empty string). May differ 
-# depending on your rewrite rules.
-#gb::$index_prefix = '';
-# Note that if you use lighttpd, read this: "Configuring PHP"
-# http://redmine.lighttpd.net/projects/lighttpd/wiki/Docs:ModFastCGI#FastCGI-and-Programming-Languages
+# Index prefix -- rewrite rules only engaged live
+if ($_SERVER['SERVER_NAME'] === 'python-smisk.org')
+	gb::$index_prefix = '';
 ?>
